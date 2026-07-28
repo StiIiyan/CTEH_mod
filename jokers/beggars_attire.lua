ab7b62dc4cdfc469d44c650b18235d9b6eedc042
StiIiyan <ssossess@abv.bg>
2026-07-28 12:40:41 +0300

    Make joker Beggar's Attire work with multiple instances

--- jokers/beggars_attire.lua
+++ jokers/beggars_attire.lua
@@ -48,18 +48,25 @@ SMODS.Joker{
 
 local old_create_card = create_card
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    if (_type == 'Tarot' or _type == 'Tarot_Planet' and is_key_tarot(G.GAME.last_tarot_planet)) and next(SMODS.find_card("j_CTEH_beggars_attire")) and
-        not soulable and key_append ~= "sho" and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-            create_additional_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    if next(SMODS.find_card("j_CTEH_beggars_attire")) then
+            create_additional_tarots(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
     end
     
     return old_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
 end
 
-function create_additional_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    if area ~= G.consumeables and #G.consumeables.cards + G.GAME.consumeable_buffer >= G.consumeables.config.card_limit then return end
+function create_additional_tarots(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    local beggars_attire_condition = ((_type == 'Tarot' or _type == 'Tarot_Planet' and is_key_tarot(G.GAME.last_tarot_planet))
+        and not soublable and key_append ~= "sho" and area == G.consumeables 
+        and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit)
+
+    if not beggars_attire_condition then return end
+
+    local additional_cards = math.min((G.consumeables.config.card_limit - (#G.consumeables.cards + G.GAME.consumeable_buffer)),#SMODS.find_card('j_CTEH_beggars_attire'))
     
-    local card = old_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    card:add_to_deck()
-    area:emplace(card)
+    for i = 1, additional_cards do
+        local card = old_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+        card:add_to_deck()
+        area:emplace(card)
+    end
 end
