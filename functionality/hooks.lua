c3402aa6612ecf1143dc7bef1a0874bc808045bf
StiIiyan <ssossess@abv.bg>
2026-07-31 09:07:13 +0300

    Relocations of hooks and miscellaneous functions

--- functionality/hooks.lua
+++ functionality/hooks.lua
@@ -25,18 +25,48 @@ function create_card(_type, area, legendary, _rarity, skip_materialize, soulable
     return old_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
 end
 
-function create_additional_tarots(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    local beggars_attire_condition = ((_type == 'Tarot' or _type == 'Tarot_Planet' and is_key_tarot(G.GAME.last_tarot_planet))
-        and not soublable and key_append ~= "sho" and area == G.consumeables 
-        and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit)
+-- Custom wincon, custom winning condition
+local game_start_run_ref = Game.start_run
+function Game:start_run(args)
+    game_start_run_ref(self, args)
 
-    if not beggars_attire_condition then return end
-
-    local additional_cards = math.min((G.consumeables.config.card_limit - (#G.consumeables.cards + G.GAME.consumeable_buffer)),#SMODS.find_card('j_CTEH_beggars_attire'))
-    
-    for i = 1, additional_cards do
-        local card = old_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-        card:add_to_deck()
-        area:emplace(card)
-    end
+    G.GAME.win_ante = G.GAME.modifiers.alternate_wincon and 39 or G.GAME.win_ante
 end
+
+-- Make sizhancement indestructable
+local old_shatter = Card.shatter
+function Card:shatter()
+    if self.config.center.key ~= 'm_CTEH_sizaak_enh' then 
+        old_shatter(self) 
+        return 
+    end
+    
+    local force_lose = false
+    if G.STATE ~= G.STATES.HAND_PLAYED then force_lose = true end
+
+    old_shatter(self)
+    siz_end_game(force_lose)
+end
+
+-- Make sizhancement unable to be overridden
+local old_set_ability = Card.set_ability
+function Card:set_ability(center, initial, delay_sprites)
+    if G.STATE ~= G.STATES.MENU and G.STATE ~= G.STATES.BLIND_SELECT and self.config.center.key == 'm_CTEH_sizaak_enh' then return end
+    old_set_ability(self, center, initial, delay_sprites)
+end
+
+-- Make sizhancement rank unchangeable
+local old_set_base = Card.set_base
+function Card:set_base(card, initial)-- some weird signature shi, a whole convo in discord
+    if G.STATE ~= G.STATES.MENU and G.STATE ~= G.STATES.BLIND_SELECT and self.config.center.key == 'm_CTEH_sizaak_enh' then return end
+    old_set_base(self, card, initial)
+end
+
+-- Make sizhancement suit unchangeable
+local old_change_suit = Card.change_suit
+function Card:change_suit(new_suit)
+    if G.STATE ~= G.STATES.MENU and G.STATE ~= G.STATES.BLIND_SELECT and self.config.center.key == 'm_CTEH_sizaak_enh' then return end
+    old_change_suit(self, new_suit)
+end
+
+-- TODO (not priority) make siz edition unchangeable
--- functionality/misc.lua
+++ functionality/misc.lua
@@ -53,9 +53,43 @@ SMODS.ObjectType({
     }
 })
 
-local game_start_run_ref = Game.start_run
-function Game:start_run(args)
-    game_start_run_ref(self, args)
 
-    G.GAME.win_ante = G.GAME.modifiers.alternate_wincon and 39 or G.GAME.win_ante
+function create_additional_tarots(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    local beggars_attire_condition = ((_type == 'Tarot' or _type == 'Tarot_Planet' and is_key_tarot(G.GAME.last_tarot_planet))
+        and not soublable and key_append ~= "sho" and area == G.consumeables 
+        and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit)
+
+    if not beggars_attire_condition then return end
+
+    local additional_cards = math.min((G.consumeables.config.card_limit - (#G.consumeables.cards + G.GAME.consumeable_buffer)),#SMODS.find_card('j_CTEH_beggars_attire'))
+    
+    for i = 1, additional_cards do
+        local card = old_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+        card:add_to_deck()
+        area:emplace(card)
+    end
+end
+
+
+-- Custom wincon, sizaak win condition
+function siz_end_game(force_lose)
+    local enh_cards = 0
+    for _, playing_card in pairs(G.playing_cards or {}) do
+        if next(SMODS.get_enhancements(playing_card)) then enh_cards = enh_cards + 1 end
+    end
+    -- WIN STEP 1: CRASH BUS                (for winning play at least 1 hand for bus to reset)
+    if BUS_JUST_CRASHED and enh_cards == 16 and not force_lose then --15 after removing, allegedly
+        -- loses instead of W at (== 15), wins instead of L at (== 16) eventhough oldShatter calls before this function
+        win_game()
+    else
+        -- end_round() isn't necessarily lose_game() -> game doesn't end if it's end of round (played in winning hand)
+        
+        G.STATE = G.STATES.GAME_OVER
+        if not G.GAME.won and not G.GAME.seeded and not G.GAME.challenge then 
+            G.PROFILES[G.SETTINGS.profile].high_scores.current_streak.amt = 0
+        end
+        G:save_settings()
+        G.FILE_HANDLER.force = true
+        G.STATE_COMPLETE = false
+    end
 end
